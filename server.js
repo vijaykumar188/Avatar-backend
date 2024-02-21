728a27e1e8b0e36bb502f681d3ae240981dbe2ba
vijaykumar188 <vijisnv97@gmail.com>
2024-02-21 14:28:56 +0530

    mongo-URL-changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const morgan = require('morgan')
 const mongoose = require('mongoose');
 const dotenv = require('dotenv')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 6000
 const images = require('./routes/uploadRoute')
 
 //Allow access to .env file
